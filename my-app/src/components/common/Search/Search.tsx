ab4bda34d8a51dd88c718ebdedec57e5bae2929d
Bartłomiej Lańczyk <bartlomiej.lanczyk@msleopard.com>
2021-06-13 17:10:25 +0200

    Delete css module.

--- my-app/src/components/common/Search/Search.tsx
+++ my-app/src/components/common/Search/Search.tsx
@@ -1,8 +1,9 @@
 import React, { Component, RefObject } from 'react';
 
 import { ImSearch } from "react-icons/im";
-import cx from 'classnames';
-import styles from "./Search.module.scss";
+import styled from 'styled-components';
+import { Sizes } from '../../../styledHelpers/Sizes';
+import { Colors } from '../../../styledHelpers/Colors';
 
 type S = {
     value: string
@@ -14,6 +15,52 @@ type P = {
     onSearchClick: Function,
     onChange: Function
 }
+
+const SearchContainer = styled.div`
+position: relative;
+overflow: hidden;
+height: 2rem;
+`;
+
+const InputContainer = styled.input`
+font-family: Arial, Helvetica, sans-serif;
+box-sizing: border-box;
+width: 100%;
+height: 100%;
+padding-right: ${Sizes.spacing5};
+padding-left: ${Sizes.spacing2};
+transition: border ease-in 0.14s;
+border: 1px solid #d3d3d3;
+border-radius: 4px;
+
+&:focus {
+    border: 1px solid ${Colors.black};
+    outline: 0;
+}
+`;
+
+const SearchButton = styled.button`
+font-size: 1rem;
+position: absolute;
+top: 0;
+right: 0;
+display: flex;
+align-items: center;
+justify-content: center;
+width: 2.5rem;
+height: 100%;
+cursor: pointer;
+transition: background 0.24s, color 0.12s;
+color: ${Colors.black};
+border: none;
+border-radius: 0 4px 4px 0;
+background: transparent;
+&:focus {
+    color: ${Colors.black};
+    border: 1px solid ${Colors.black};
+    outline: 0;
+}
+`;
 
 class Search extends Component<P, S> {
     static defaultProps: P = {
@@ -48,12 +95,12 @@ class Search extends Component<P, S> {
 
     render() {
         return (
-            <div className={cx(styles.Search, this.props.customClass)}>
-                <input ref={this.searchInput} type="text" value={this.state.value} placeholder={this.props.placeholder} onChange={(ev) => this.onChange(ev)} />
-                <button type="button" className={cx(styles.SearchButton)} onClick={(ev) => this.props.onSearchClick(ev)}>
-                    <ImSearch className={styles.SearchIcon} />
-                </button>
-            </div>
+            <SearchContainer>
+                <InputContainer ref={this.searchInput} type="text" value={this.state.value} placeholder={this.props.placeholder} onChange={(ev) => this.onChange(ev)} />
+                <SearchButton type="button" onClick={(ev) => this.props.onSearchClick(ev)}>
+                    <ImSearch />
+                </SearchButton>
+            </SearchContainer>
         );
     }
 }
